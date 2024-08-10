d72f1e5dcb0a7c883844e603fbdf1b131d6c097d
ReadonIII <96930418+ReadonIII@users.noreply.github.com>
2024-08-10 14:46:41 -0600

    Add files via upload

--- h2InfrasApp.py
+++ h2InfrasApp.py
@@ -41,9 +41,8 @@ def load_data():
                      {'IEA zero-carbon estimated normalized capacity[nm³ H₂/hour]':
                       'Zero-carbon norm. capacity[nm³ H₂/hour]'})
     #.........for consistency all: FID, etc-> "Under Construction", Unknown, etc->Other 
-    data.replace(to_replace=["FID/Construction", "FID"],value="Under Construction")
-    data.replace(to_replace=["Other/Unknown", "Unknown"],value="Other")
-    st.write(data)
+    data.replace(to_replace="FID/Construction",value="Under Construction")
+    data.replace(to_replace=["Other/Unknown", "Unknown"],value="Other")    
     #----read country list and create dictionary for it and country code
     #.....for UX: keys-> Country name and values->ISO Code
     country_list=pd.read_excel(datafilename,
